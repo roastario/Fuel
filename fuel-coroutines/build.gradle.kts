c071f40285f6a485d6cc1b6035c604899646c058
MarkGilchrist <theheadchef@gameforeverything.com>
2018-10-03 08:09:40 +0100

    moving over to kotlin 1.3 not working

--- fuel-coroutines/build.gradle.kts
+++ fuel-coroutines/build.gradle.kts
@@ -1,16 +1,32 @@
-import com.novoda.gradle.release.PublishExtension
+
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins { java }
+plugins { java
+    kotlin("jvm") version "1.3.0-rc-116"
+}
 
 dependencies {
     compile(project(":fuel"))
-    compile(Dependencies.kotlinCoroutinesJvm)
+    compile(project( Dependencies.kotlinCoroutinesJvm))
     testCompile(Dependencies.mockServer)
+    compile(kotlin("stdlib-jdk8"))
 }
 
 configure<KotlinJvmProjectExtension> {
     experimental.coroutines = Coroutines.ENABLE
+}
+repositories {
+    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
